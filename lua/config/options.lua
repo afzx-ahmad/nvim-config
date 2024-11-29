825fded228381e32d9941a9acaab8461f38161aa
afzx-ahmad <afzx.ahmad@gmail.com>
2024-11-30 00:25:39 +0700

    Adding clear highlight keymap, and none_ls

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,12 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<ESC>", function()
+        if(vim.v.hlsearch == 1) then
+            vim.cmd("nohlsearch")
+        else
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<ESC>", true, false, true), "n", false)
+        end
+    end, { desc = "Clear search highlight" }
+)
