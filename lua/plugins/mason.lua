1981f71d25e02421b5820ce376e45d2b909f8856
afzx-ahmad <afzx.ahmad@gmail.com>
2024-11-30 01:15:31 +0700

    Adding completion and snippet

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -18,10 +18,17 @@ return {
         config = function()
             local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({})
-            lspconfig.cssls.setup({})
-            lspconfig.tailwindcss.setup({})
-            lspconfig.eslint.setup({})
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.cssls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.tailwindcss.setup({
+                capabilities = capabilities
+            })
 
             vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
         end
